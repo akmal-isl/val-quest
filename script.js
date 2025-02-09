bac00a096a355418585d51a963b784eeeac32bbb
ak1.one <71212957+akmal-isl@users.noreply.github.com>
2025-02-09 21:07:02 +0500

    Update script.js

--- script.js
+++ script.js
@@ -9,22 +9,6 @@ document.getElementById('music-btn').addEventListener('click', function () {
     }
   });
   
-  // Функция для создания падающих сердечек
-  function createHeart() {
-    const heart = document.createElement('div');
-    heart.classList.add('heart');
-    heart.innerHTML = '❤️';
-    heart.style.left = Math.random() * 100 + "vw";
-    heart.style.animationDuration = Math.random() * 2 + 3 + "s";
-    document.body.appendChild(heart);
-  
-    setTimeout(() => {
-      heart.remove();
-    }, 5000);
-  }
-  
-  setInterval(createHeart, 300);
-  
 function checkCode() {
     var input = document.getElementById("codeInput").value;
     var errorMessage = document.getElementById("errorMessage");
